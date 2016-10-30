39c4499c751f5c284f95500a7827e518892a3e54
jiangkun <jiangkun@bytedance.com>
2016-10-30 20:34:29 +0800

    add splash

---
+++ app/src/main/java/com/kun/news/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.kun.news;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.kun.news.common.activty.BaseActivity;
+import com.kun.news.main.MainActivity;
+
+/**
+ * Created by jiangkun on 2016/10/30.
+ */
+
+public class SplashActivity extends Activity {
+
+    private void redirect() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_splash);
+        findViewById(R.id.splash_view).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                redirect();
+            }
+        }, 800);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        redirect();
+    }
+
+}
