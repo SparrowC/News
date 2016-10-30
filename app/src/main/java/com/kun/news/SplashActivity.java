68dc41a1b0f986c058674643511e8d6a1e039440
jiangkun <jiangkun@bytedance.com>
2016-10-30 20:58:28 +0800

    add rendering

--- app/src/main/java/com/kun/news/SplashActivity.java
+++ app/src/main/java/com/kun/news/SplashActivity.java
@@ -3,9 +3,7 @@ package com.kun.news;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
-import com.kun.news.common.activty.BaseActivity;
 import com.kun.news.main.MainActivity;
 
 /**
